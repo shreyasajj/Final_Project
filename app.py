c3cfd9ec56a762c5b114649b8f5b86e86ecaaa6b
shreyasajj <shreyasajj@tamu.edu>
2020-04-20 23:15:48 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('ImageLoad',METHOD="PUT")
+def ImageLoad():
+
+
+if __name__ == '__main__':
+    app.run()
